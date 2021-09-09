385faf8628f9c86eb6259a52c274a05e63615f77
Lalith Kumar <lalith.nimmala@gmail.com>
2021-09-09 17:59:28 -0600

    Test

--- Problem_001/Sol001.py
+++
@@ -1,9 +0,0 @@
-def sum_multiple():
-    total = 0
-    for i in range(3,1001):
-        if (i % 3 == 0) or (i % 5 == 0):
-            total = total + i
-    return(total)
-
-
-print(sum_multiple())
