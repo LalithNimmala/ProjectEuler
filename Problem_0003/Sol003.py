32a1933c734536e380b12331290efc6b988b5b0b
Lalith Kumar <lalith.nimmala@gmail.com>
2021-09-10 08:38:50 -0600

    Test

--- Problem_0003/Sol003.py
+++ Problem_0003/Sol003.py
@@ -1,4 +1,12 @@
+"""
+Problem 3
+The prime factors of 13195 are 5, 7, 13 and 29.
 
+What is the largest prime factor of the number 600851475143 ?
+
+
+Answer:  6857
+"""
 def is_prime(x):
     for i in range(2, x):
         if x % i == 0:
--- snippets.py
+++ snippets.py
@@ -1,2 +1,11 @@
-for i in range(1, 10):
-    print(i)
+def is_palindrome(x):
+    return(True)
+
+
+print(str(100))
+list = []
+
+for i in range(0, len(str(100))):
+    list.append(int(str(100)[i]))
+
+print(list)
